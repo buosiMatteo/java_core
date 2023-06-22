f2afd0195193777cf80daba6d6a262a4c32e8fed
xWolf <buosi.matteo@gmail.com>
2023-06-22 17:15:44 +0200

    modified HelloWorld.java with an even/odd program

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
+
 public class HelloWorld {
 
   public static void main(String[] args) {
-
-      System.out.println("Hello World I'm " + args[0]);
+    //Scanner tastiera = new Scanner(System.in);
+    //System.out.println("Inserisci un numero: ");
+    //String x = tastiera.nextLine();
+    int y = Integer.parseInt(args[0]);
+    if(y % 2 == 0){
+      System.out.println("The number is even");
+    }  else {
+      System.out.println("The number is odd");
+    }
+    //tastiera.close();
+    //System.out.println("");
 
   }
 
